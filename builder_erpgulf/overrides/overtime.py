1a15232b596467cdc523606174d9d5c799c39fef
Harshafiroz <harsha@htsqatar.com>
2026-06-02 13:11:37 +0300

    attendance fix

--- builder_erpgulf/overrides/overtime.py
+++ builder_erpgulf/overrides/overtime.py
@@ -10,46 +10,98 @@ class Attendance(HRMSAttendance):
 		self.calculate_overtime_hours()
 
 	def calculate_overtime_hours(self):
+    self.overtime_hours = 0
 
-		self.overtime_hours = 0
+    if not (self.shift and self.in_time and self.out_time):
+        return
 
-		if not (self.shift and self.in_time and self.out_time):
-			return
+    shift = frappe.get_doc("Shift Type", self.shift)
 
-		shift = frappe.get_doc("Shift Type", self.shift)
+    if not shift.enable_overtime_calculation:
+        return
 
-		if not shift.enable_overtime_calculation:
-			return
+    in_time = get_datetime(self.in_time)
+    out_time = get_datetime(self.out_time)
 
-		attendance_date = get_datetime(self.attendance_date)
+    attendance_date = get_datetime(self.attendance_date)
 
-		start_seconds = shift.start_time.total_seconds()
-		end_seconds = shift.end_time.total_seconds()
+    start_seconds = shift.start_time.total_seconds()
+    end_seconds = shift.end_time.total_seconds()
 
-		start_hour = int(start_seconds // 3600)
-		start_min = int((start_seconds % 3600) // 60)
+    start_hour = int(start_seconds // 3600)
+    start_min = int((start_seconds % 3600) // 60)
 
-		end_hour = int(end_seconds // 3600)
-		end_min = int((end_seconds % 3600) // 60)
+    end_hour = int(end_seconds // 3600)
+    end_min = int((end_seconds % 3600) // 60)
 
-		shift_start = attendance_date.replace(
-			hour=start_hour,
-			minute=start_min,
-			second=0
-		)
+    shift_start = attendance_date.replace(
+        hour=start_hour,
+        minute=start_min,
+        second=0,
+        microsecond=0
+    )
 
-		shift_end = attendance_date.replace(
-			hour=end_hour,
-			minute=end_min,
-			second=0
-		)
+    shift_end = attendance_date.replace(
+        hour=end_hour,
+        minute=end_min,
+        second=0,
+        microsecond=0
+    )
 
-		overtime = 0
+    if shift_end <= shift_start:
+        shift_end = frappe.utils.add_days(shift_end, 1)
 
-		if self.in_time < shift_start:
-			overtime += time_diff_in_hours(shift_start, self.in_time)
+    overtime = 0
 
-		if self.out_time > shift_end:
-			overtime += time_diff_in_hours(self.out_time, shift_end)
+    if in_time < shift_start:
+        overtime += time_diff_in_hours(shift_start, in_time)
 
-		self.overtime_hours = round(overtime, 2)
+    if out_time > shift_end:
+        overtime += time_diff_in_hours(out_time, shift_end)
+
+    self.overtime_hours = round(overtime, 2)
+
+	# def calculate_overtime_hours(self):
+
+	# 	self.overtime_hours = 0
+
+	# 	if not (self.shift and self.in_time and self.out_time):
+	# 		return
+
+	# 	shift = frappe.get_doc("Shift Type", self.shift)
+
+	# 	if not shift.enable_overtime_calculation:
+	# 		return
+
+	# 	attendance_date = get_datetime(self.attendance_date)
+
+	# 	start_seconds = shift.start_time.total_seconds()
+	# 	end_seconds = shift.end_time.total_seconds()
+
+	# 	start_hour = int(start_seconds // 3600)
+	# 	start_min = int((start_seconds % 3600) // 60)
+
+	# 	end_hour = int(end_seconds // 3600)
+	# 	end_min = int((end_seconds % 3600) // 60)
+
+	# 	shift_start = attendance_date.replace(
+	# 		hour=start_hour,
+	# 		minute=start_min,
+	# 		second=0
+	# 	)
+
+	# 	shift_end = attendance_date.replace(
+	# 		hour=end_hour,
+	# 		minute=end_min,
+	# 		second=0
+	# 	)
+
+	# 	overtime = 0
+
+	# 	if self.in_time < shift_start:
+	# 		overtime += time_diff_in_hours(shift_start, self.in_time)
+
+	# 	if self.out_time > shift_end:
+	# 		overtime += time_diff_in_hours(self.out_time, shift_end)
+
+	# 	self.overtime_hours = round(overtime, 2)
